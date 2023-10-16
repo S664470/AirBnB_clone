36ef458fac673bf3e2e359fbc9da22ce8b601ea1
S664470 <shahialtayeb@gmail.com>
2023-10-16 09:28:28 -0700

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-"""
-This contain the BaseModel that define all common attributes and methods
-"""
+"""BaseModel"""
 import uuid
-from datetime import datetime
-
+import datetime
 
 class BaseModel:
-    """public instance attribute"""
+    """class attribute"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, **kwargs):
+        self.id = kwargs.get('id')
+        self.created_at = kwargs.get('created_at')
+        self.updated_at = kwargs.get('updated_at')
+        self.name = kwargs.get('name')
+        self.my_number = kwargs.get('my_number')
 
     def save(self):
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[BaseModel] ({self.id}) {self.to_dict()}"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'name': self.name,
+            'my_number': self.my_number,
+            '__class__': self.__class__.__name__,
+        }
